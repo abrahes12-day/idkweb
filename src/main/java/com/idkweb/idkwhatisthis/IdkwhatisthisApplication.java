0a2605492a3bef3e19bf11d7635ab6852dddbbbe
Yhudi Cornelius <abrahes12@yahoo.com>
2022-03-19 11:19:56 +0700

    Initial Commit

---
+++ src/main/java/com/idkweb/idkwhatisthis/IdkwhatisthisApplication.java
@@ -0,0 +1,13 @@
+package com.idkweb.idkwhatisthis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdkwhatisthisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdkwhatisthisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idkweb/idkwhatisthis/IdkwhatisthisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idkweb.idkwhatisthis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdkwhatisthisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
